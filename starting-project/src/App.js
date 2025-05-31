893f243a062f2c0f41d12296ca9445940312b2b0
SayuruDev <mailtosayuru@gmail.com>
2025-05-31 08:57:42 -0400

    Error Page

--- starting-project/src/App.js
+++ starting-project/src/App.js
@@ -1,19 +1,35 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  // createRoutesFromElements,
+  RouterProvider,
+  // Route,
+} from 'react-router-dom';
 
-import HomePage from "./pages/Home";
-import ProductsPage from "./pages/Products";
-import RootLayout from "./pages/Root";
+import ErrorPage from './pages/Error';
+import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
-      { path: "/", element: <HomePage /> },
-      { path: "/products", element: <ProductsPage /> },
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> },
     ],
-  },
+  }
 ]);
+
+// const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
